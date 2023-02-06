c049968bed5164fc536aae55e6f1fcc3948343a7
DAN3002 <dinhanh300229@gmail.com>
2023-02-06 15:41:25 +0700

    Finish swal + bookmarklet

--- index.js
+++ index.js
@@ -1,143 +1,178 @@
-const ALLOW_TAG = ['div', 'section', 'article', 'p', 'span', 'a', 'ul', 'li', 'ol', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'img', 'table', 'tr', 'td', 'th', 'thead', 'tbody', 'tfoot', 'caption', 'colgroup', 'col', 'label', 'header', 'footer', 'nav', 'main', 'aside', 'article', 'details', 'summary', 'head', 'body', 'html', 'address', 'blockquote', 'pre', 'code',];
-const CONTAINER_TAG = ['div', 'section', 'article', 'ul', 'ol', 'img', 'table', 'header', 'footer', 'nav', 'main', 'aside', 'article', 'details', 'summary', 'body', 'html', 'address', ];
-const INDENT_SIZE = 4;
+// bookmarlet
+javascript: (function () {
+	const ALLOW_TAG = ['div', 'section', 'article', 'p', 'span', 'a', 'ul', 'li', 'ol', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'img', 'table', 'tr', 'td', 'th', 'thead', 'tbody', 'tfoot', 'caption', 'colgroup', 'col', 'label', 'header', 'footer', 'nav', 'main', 'aside', 'article', 'details', 'summary', 'head', 'body', 'html', 'address', 'blockquote', 'pre', 'code', ];
+	const CONTAINER_TAG = ['div', 'section', 'article', 'ul', 'ol', 'img', 'table', 'header', 'footer', 'nav', 'main', 'aside', 'article', 'details', 'summary', 'body', 'html', 'address', ];
+	const INDENT_SIZE = 4;
 
-const createTagPath = (node) => {
-	let out = node.tagName.toLowerCase();
+	const createTagPath = (node) => {
+		let out = node.tagName.toLowerCase();
 
-	if (node.id) {
-		out += `#${node.id}`;
-	}
-	else if (node.className) {
-		const classStr = node.className.split(' ').join('.');
-		out += `.${classStr}`;
-	}
+		if (node.id) {
+			out += `#${node.id}`;
+		} else if (node.className) {
+			const classStr = node.className.split(' ').join('.');
+			out += `.${classStr}`;
+		}
 
-	return out;
-};
+		return out;
+	};
 
-const traverse = (node) => {
-	let { children } = node;
-	
-	// convert children to array
-	children = Array.from(children);
-	
-	// Filter children by ALLOW_TAG
-	children = children.filter((child) => ALLOW_TAG.includes(child.tagName.toLowerCase()));
+	const traverse = (node) => {
+		let {
+			children
+		} = node;
 
-	const child = [];
-	
-	for (childNode of children) {
-		child.push(traverse(childNode));
-	}
+		// convert children to array
+		children = Array.from(children);
 
-	return { path: createTagPath(node), child };
-};
+		// Filter children by ALLOW_TAG
+		children = children.filter((child) => ALLOW_TAG.includes(child.tagName.toLowerCase()));
 
-const removeDuplicate = (node) => {
-	const { child } = node;
-	const pathSet = new Set();
-	const newChild = [];
+		const child = [];
 
-	for (childNode of child) {
-		const { path } = childNode;
-		if (!pathSet.has(path)) {
-			pathSet.add(path);
-			newChild.push(childNode);
+		for (childNode of children) {
+			child.push(traverse(childNode));
+		}
+
+		return {
+			path: createTagPath(node),
+			child
+		};
+	};
+
+	const removeDuplicate = (node) => {
+		const {
+			child
+		} = node;
+		const pathSet = new Set();
+		const newChild = [];
+
+		for (childNode of child) {
+			const {
+				path
+			} = childNode;
+			if (!pathSet.has(path)) {
+				pathSet.add(path);
+				newChild.push(childNode);
+			}
+		}
+
+		node.child = newChild;
+
+		for (childNode of node.child) {
+			removeDuplicate(childNode);
+		}
+	};
+
+	// dfs structure, if node is leaf then group that node with its parent
+	// newPath = parrentPath + " >> " + nodePath
+	const dfs = (node) => {
+		let {
+			child,
+			parent
+		} = node;
+
+		for (childNode of child) {
+			dfs(childNode);
+		}
+
+		if (child.length === 0 && parent && parent.child.length == 1) {
+			// leaf node
+			parent.path += ` >> ${node.path}`;
+			parent.child = [];
+
+			dfs(parent)
+		}
+	};
+
+	const addParentToNode = (node) => {
+		const {
+			child
+		} = node;
+
+		for (childNode of child) {
+			childNode.parent = node;
+			addParentToNode(childNode);
 		}
 	}
 
-	node.child = newChild;
-	
-	for (childNode of node.child) {
-		removeDuplicate(childNode);
-	}
-};
+	let output = '';
+	const convertToStr = (node, indent) => {
+		output += `\r\n${ indent }${ node.path}`;
 
-// dfs structure, if node is leaf then group that node with its parent
-// newPath = parrentPath + " >> " + nodePath
-const dfs = (node) => {
-	let { child, parent } = node;
-
-	for (childNode of child) {
-		dfs(childNode);
+		for (const childEl of node.child) {
+			convertToStr(childEl, indent + ' '.repeat(INDENT_SIZE));
+		}
 	}
 
-	if (child.length === 0 && parent && parent.child.length == 1) {
-		// leaf node
-		parent.path += ` >> ${node.path}`;
-		parent.child = [];
+	const getStructure = (root) => {
+		// Step 1: Traverse the DOM
+		let structure = traverse(root);
 
-		dfs(parent)
+		// Step 2: Remove duplicate path in child (keep the first one)
+		removeDuplicate(structure);
+
+		// Step 3: Add parrent to each node
+		addParentToNode(structure);
+
+		// Step 4: dfs structure, if node is leaf then group that node with its parent
+		dfs(structure);
+
+		// Step 5: Convert to string
+		convertToStr(structure, '');
+
+		output = output.trim();
+
+		return output;
+	};
+
+	// Add swal lib to show alert by append new script tag, link tag
+	const swalScript = document.createElement('script');
+	swalScript.src = 'https://unpkg.com/sweetalert/dist/sweetalert.min.js';
+	document.body.appendChild(swalScript);
+
+	const swalLink = document.createElement('link');
+	swalLink.rel = 'stylesheet';
+	swalLink.href = 'https://unpkg.com/sweetalert/dist/sweetalert.css';
+	document.head.appendChild(swalLink);
+
+	// Add event listener to CONTAINER_TAG element
+	for (const tag of CONTAINER_TAG) {
+		const elements = document.getElementsByTagName(tag);
+
+		for (const el of elements) {
+			el.addEventListener('click', (e) => {
+				e.stopPropagation();
+				const structure = getStructure(el);
+
+				// Swal alert
+				swal({
+					title: 'Structure',
+					text: structure,
+					icon: 'success',
+					button: 'Copy to clipboard',
+				}).then((value) => {
+					if (value) {
+						navigator.clipboard.writeText(structure);
+					}
+				});
+
+			});
+
+			// add hover event and add red border
+			// if unhover then remove border
+			el.addEventListener('mouseover', (e) => {
+				e.stopPropagation();
+				el.style.border = '1px solid red';
+			});
+
+			el.addEventListener('mouseout', (e) => {
+				e.stopPropagation();
+				el.style.border = '';
+			});
+		}
 	}
-};
 
-const addParentToNode = (node) => {
-	const { child } = node;
+	alert('Script loaded');
 
-	for (childNode of child) {
-		childNode.parent = node;
-		addParentToNode(childNode);
-	}
-}
-
-let output = '';
-const convertToStr = (node, indent) => {
-	output += `\r\n${ indent }${ node.path}`;
-
-	for(const childEl of node.child) {
-		convertToStr(childEl, indent + ' '.repeat(INDENT_SIZE));
-	}
-}
-
-const getStructure = (root) => {
-	// Step 1: Traverse the DOM
-	let structure = traverse(root);
-
-	// Step 2: Remove duplicate path in child (keep the first one)
-	removeDuplicate(structure);
-
-	// Step 3: Add parrent to each node
-	addParentToNode(structure);
-
-	// Step 4: dfs structure, if node is leaf then group that node with its parent
-	dfs(structure);
-
-	// Step 5: Convert to string
-	convertToStr(structure, '');
-
-	output = output.trim();
-
-	return output;
-};
-
-// Add event listener to CONTAINER_TAG element
-for (const tag of CONTAINER_TAG) {
-	const elements = document.getElementsByTagName(tag);
-
-	for (const el of elements) {
-		el.addEventListener('click', (e) => {
-			e.stopPropagation();
-			const structure = getStructure(el);
-			
-			// copy to clipboard and overwrite
-			// navigator.clipboard.writeText(structure);
-			// // show alert
-			// alert('Copied to clipboard');
-
-			alert(structure);
-		});
-
-		// add hover event and add red border
-		// if unhover then remove border
-		el.addEventListener('mouseover', (e) => {
-			e.stopPropagation();
-			el.style.border = '1px solid red';
-		});
-
-		el.addEventListener('mouseout', (e) => {
-			e.stopPropagation();
-			el.style.border = '';
-		});
-	}
-}
+})();
